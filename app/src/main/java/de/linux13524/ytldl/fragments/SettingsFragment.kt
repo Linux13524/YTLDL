2efad9335508fb3370e71986c90263437e5bb57c
Linus Klöckner <linus.kloeckner@googlemail.com>
2019-09-03 12:37:34 +0200

    Sync global download options with SettingsFragment

--- app/src/main/java/de/linux13524/ytldl/fragments/SettingsFragment.kt
+++ app/src/main/java/de/linux13524/ytldl/fragments/SettingsFragment.kt
@@ -1,15 +1,28 @@
 package de.linux13524.ytldl.fragments
 
 
+import android.content.SharedPreferences
+import de.linux13524.ytldl.utils.PreferencesManager.syncPreferencesWithGlobalDownloadOptions
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
-
 import de.linux13524.ytldl.R
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+    override fun onSharedPreferenceChanged(p: SharedPreferences?, prefName: String?) {
+        activity?.syncPreferencesWithGlobalDownloadOptions()
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
-        //addPreferencesFromResource(R.xml.preferences)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+        super.onPause()
     }
 }
